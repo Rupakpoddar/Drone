d1fcdff4282ee8b2ada0c45afb8283b51b402e11
Rupak Poddar <34826905+Rupakpoddar@users.noreply.github.com>
2021-05-21 12:59:37 +0530

    Add files via upload

--- PCB_main/PCB_main.ino
+++ PCB_main/PCB_main.ino
@@ -10,11 +10,13 @@
 #define ROLL A3
 #define AUX1 5
 #define AUX2 6
+#define PADDING 10
 
 SoftwareSerial HC12(3, 2);
 Adafruit_SSD1306 display(128, 64);
 
 unsigned long lastReceiveTime = 0;
+int meanVal[4];
 
 int throttle = 512;
 int yaw = 512;
@@ -42,14 +44,77 @@ void setup() {
   Serial.begin(9600);
   HC12.begin(9600);
   delay(100);
+
+  // Calibrate Joysticks
+  for(int i = 0; i < 4; i++){
+    int sum = 0;
+    for(int j = 0; j < 5; j++){
+      if(i == 0){
+        sum += analogRead(THROTTLE);
+      }
+      else if(i == 1){
+        sum += analogRead(YAW);
+      }
+      else if(i == 2){
+        sum += analogRead(PITCH);
+      }
+      else{
+        sum += analogRead(ROLL);
+      }
+    }
+    meanVal[i] = sum/5;
+    if( i == 1 || i == 3){
+      meanVal[i] = 1023 - meanVal[i]; 
+    }
+  }
 }
 
 void loop() {
   // Analog Read
   throttle = analogRead(THROTTLE);
+  if(throttle < (meanVal[0] - PADDING)){
+    throttle = map(throttle, 0, meanVal[0], 0, 512);
+  }
+  else if(throttle > (meanVal[0] + PADDING)){
+    throttle = map(throttle, meanVal[0], 1023, 512, 1023);
+  }
+  else{
+    throttle = 512;
+  }
+  
   yaw = 1023 - analogRead(YAW);
+  if(yaw < (meanVal[1] - PADDING)){
+    yaw = map(yaw, 0, meanVal[1], 0, 512);
+  }
+  else if(yaw > (meanVal[1] + PADDING)){
+    yaw = map(yaw, meanVal[1], 1023, 512, 1023);
+  }
+  else{
+    yaw = 512;
+  }
+  
   pitch = analogRead(PITCH);
+  if(pitch < (meanVal[2] - PADDING)){
+    pitch = map(pitch, 0, meanVal[2], 0, 512);
+  }
+  else if(pitch > (meanVal[2] + PADDING)){
+    pitch = map(pitch, meanVal[2], 1023, 512, 1023);
+  }
+  else{
+    pitch = 512;
+  }
+  
   roll = 1023 - analogRead(ROLL);
+  if(roll < (meanVal[3] - PADDING)){
+    roll = map(roll, 0, meanVal[3], 0, 512);
+  }
+  else if(roll > (meanVal[3] + PADDING)){
+    roll = map(roll, meanVal[3], 1023, 512, 1023);
+  }
+  else{
+    roll = 512;
+  }
+  
   aux1 = digitalRead(AUX1);
 
   // Transmit
@@ -75,28 +140,7 @@ void loop() {
   display.fillRect(8*display.width()/12, map(aux1, 0, 1, display.height(), 0), display.width()/12, display.height(), SSD1306_INVERSE);
   display.drawRect(10*display.width()/12, 0, display.width()/12, display.height(), SSD1306_INVERSE);
   display.display();
-  /*
-  display.clearDisplay();
-  display.setCursor(0,15);
-  display.print("T:");
-  display.print(throttle);
-  display.print(" ");
-  display.print("Y:");
-  display.print(yaw);
-  display.print("\n");
-  display.print("P:");
-  display.print(pitch);
-  display.print(" ");
-  display.print("R:");
-  display.print(roll);
-  display.print("\n");
-  display.print("A1:");
-  display.print(aux1);
-  display.print(" ");
-  display.print("A2:");
-  display.print(aux2);
-  display.display();
-  */
+ 
   delay(50);  // Important delay
 
   if(HC12.available() > 0){
